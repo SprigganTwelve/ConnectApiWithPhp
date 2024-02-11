77a6bdff4d3c2c4c6ab1d2bfed1108fc6b2d1df8
Paterne <angepaterne.dali@gmail.com>
2024-02-11 15:06:41 +0100

    ..

--- PHP_SQY_25.16_ESHOP/config/php_mailer.php
+++ PHP_SQY_25.16_ESHOP/config/php_mailer.php
@@ -2,5 +2,5 @@
 define("SMTP_HOST", "smtp.gmail.com");
 define("SMTP_PORT", 587);
 define("SMTP_USERNAME", 'dalizouayoboangepaterne@gmail.com');
-define("SMTP_PASSWORD", "");
+define("SMTP_PASSWORD", "votreMotdePasse");
 define("SMTP_ENCRYPTION", "tls");
