f2133dbd9ed0167480796dac045618fd90d6cca3
Paterne <angepaterne.dali@gmail.com>
2024-03-02 03:54:17 +0100

    ..

---
+++ PHP_SQY_25.16_ESHOP/view/router.php
@@ -0,0 +1,25 @@
+<?php
+
+// recupere l'uri
+
+
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+$base = basename($uri);
+
+
+// Definir le tableau associatifs des routes 
+
+$routes = [
+    '' => 'controller/index.php',
+    'products' => 'controllers/products.php',
+    'login' => 'login.php',
+    'cart' => 'controllers/cart.php',
+    'profile' => 'controllers/profile.php',
+    'logout' => 'controllers/lagout.php',
+    'contact' => 'controllers/conttact.php',
+    'product' => 'controllers/product.php',
+];
+
+//On viendra require a bonne page 404
+
+(array_key_exists($base, $routes) ? require $routes[$uri] : require 'controllers/404.view.php');
